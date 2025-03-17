2bbd1c943445458cf95c6b1f5e2cc86a2ddd9930
mcieciora <michalcieciora96@gmail.com>
2025-03-17 10:10:47 +0100

    17032025-1010

--- tools/shell_scripts/delete_old_dockerhub_tags.sh
+++ tools/shell_scripts/delete_old_dockerhub_tags.sh
@@ -17,7 +17,7 @@ jq -c '.results[]' all_tags.json | while read -r TAG; do
   TAG_LAST_PULLED_TIMESTAMP=$(date -d "$TAG_LAST_PULLED" +%s)
   DATE_DIFF=$(( (CURRENT_DATE - TAG_LAST_PULLED_TIMESTAMP) / 86400 ))
 
-  if [ $DATE_DIFF -gt 3 ] && ! [[ "${PROTECTED_TAGS[*]}" =~ $TAG_NAME ]]; then
+  if [ $DATE_DIFF -gt "$DAYS_TO_KEEP_DOCKERHUB_IMAGES_POLICY" ] && ! [[ "${PROTECTED_TAGS[*]}" =~ $TAG_NAME ]]; then
     echo "$TAG_NAME is $DATE_DIFF days old. It will be deleted."
     curl --fail -X DELETE -H "Authorization: JWT $TOKEN" "https://hub.docker.com/v2/repositories/$DOCKERHUB_REPO/tags/$TAG_NAME/"
 
